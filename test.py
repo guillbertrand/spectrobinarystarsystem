ab3849a15ea53486d51e9ca549f43a0318a0350e
Guillaume Bertrand <gbe.io@pm.me>
2023-04-25 12:21:14 +0200

    setupconfig + rename directory

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from spectroscopicbinarysystem import SpectroscopicBinarySystem
 
 sbs = SpectroscopicBinarySystem(
-    'hd123299', './sample/alphadra/', t0=2451441.804, period_guess=51, debug=True)
+    'hd123299', './examples/alphadra/', t0=2451441.804, period_guess=51, debug=True)
 
 sbs.plotRadialVelocityCurve(
-    title="α Dra - HD123299 - Phased radial velocities", savefig=False)
+    title="α Dra - HD123299 - Phased radial velocities", savefig=True)
 
 sbs.plotlyRadialVelocityCurve(
     title="α Dra - HD123299 - Phased radial velocities")
