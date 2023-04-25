5525098e549ef54de83d54149b42f3267a3c1f30
Guillaume Bertrand <gbe.io@pm.me>
2023-04-25 11:25:34 +0200

    remove old files

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ from spectroscopicbinarysystem import SpectroscopicBinarySystem
 
 sbs = SpectroscopicBinarySystem(
     'hd123299', './sample/alphadra/', t0=2451441.804, period_guess=51, debug=True)
+
 sbs.plotRadialVelocityCurve(
-    title="α Dra - HD123299 - Phased radial velocities", savefig=True)
+    title="α Dra - HD123299 - Phased radial velocities", savefig=False)
+
+sbs.plotlyRadialVelocityCurve(
+    title="α Dra - HD123299 - Phased radial velocities")
