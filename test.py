f49e0bf40887e44cc40b5b95230d45cf84598461
Guillaume Bertrand <gbe.io@pm.me>
2023-04-25 11:15:15 +0200

    conf setuptools

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from spectroscopicbinarysystem.spectroscopicbinarysystem import SpectroscopicBinarySystem
+from spectroscopicbinarysystem import SpectroscopicBinarySystem
 
-sbs = SpectroscopicBinarySystem('hd123299','./sample/alphadra/', t0=2451441.804, period_guess=51, debug=True)
-sbs.plotRadialVelocityCurve(title="α Dra - HD123299 - Phased radial velocities",savefig=True)
+sbs = SpectroscopicBinarySystem(
+    'hd123299', './sample/alphadra/', t0=2451441.804, period_guess=51, debug=True)
+sbs.plotRadialVelocityCurve(
+    title="α Dra - HD123299 - Phased radial velocities", savefig=True)
