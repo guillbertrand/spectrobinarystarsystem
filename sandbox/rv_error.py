a3e4ddfa3c53464f6ad264cae6448f8613ed7b3f
Guillaume Bertrand <gbe.io@pm.me>
2023-05-12 16:57:15 +0200

    fix errors

--- sandbox/rv_error.py
+++ sandbox/rv_error.py
@@ -23,11 +23,11 @@ def computeConstA_Method1(rv, fwhm, snr, contrast, n):
     return rv / (fwhm / (contrast * snr * np.sqrt(n)))
 
 
-def computeConstB_Method2(rv, fwhm, snr, contrast, center):
+def computeConstB_Method2(rv, fwhm, p, snr, contrast, center):
     """
     return a constant for a given radial velocity error in km.s-1 (BOUCHY)
     """
-    return rv / ((np.sqrt(fwhm) * np.sqrt((fwhm/center)*const.c)) / (contrast * snr))
+    return rv / ((np.sqrt(fwhm) * np.sqrt(p)) / (contrast * snr))
 
 
 def findCenterOfLine(spectrum1d, fwhm, model_type='gaussian'):
@@ -114,15 +114,21 @@ def run(model, n_samples, snr):
     contrast = .68
     center = 6560.123
     fwhm = np.arange(2, 8, .5)
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 7))
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 7))
     aa, bb = [], []
     for i in range(len(fwhm)):
+        # run monteCarlo
         std = runMonteCarlo(
             n_samples, contrast, snr, center, fwhm[i], model=model, ax=ax4, first=i == 1)
+
+        # compute A constant with first method
         a = computeConstA_Method1(
-            getRV(std), fwhm[i], snr, contrast, fwhm[i]/.05)
+            getRV(std), getRV(fwhm[i]), snr, contrast, fwhm[i]/.05)
+
+        # compure B constant with second method
         b = computeConstB_Method2(
-            getRV(std), fwhm[i], snr, contrast, center)
+            getRV(std), getRV(fwhm[i]), snr, getRV(.05), contrast, center)
+
         print(f'rms : {std}  a : {a}  b : {b}')
         ax1.plot(a, fwhm[i], "ko")
         ax2.plot(b, fwhm[i], "ko")
